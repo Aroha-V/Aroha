7e083c628c4c03ee5c7cf462517a5869ae213e08
kvkdev-ml <kvkwork05@gmail.com>
2026-06-02 22:02:18 +0530

    ..

--- KVK/RAG/return_content.py
+++ KVK/RAG/return_content.py
@@ -7,7 +7,7 @@ cdb_clt = CloudClient(
     tenant=os.getenv('CHROMA_TENANT'),
     database='ADB'
 )
-df = pd.read_csv(r'D:\KVKDEV\RAG\idsp_data_final.csv')
+df = pd.read_csv(rf'{os.getcwd()}\AROHA_PROJECT\Aroha\KVK\RAG\idsp_data_final.csv')
 def return_context(ip: str, collection_name: str = 'AROHA_data') -> str:
    coll=cdb_clt.get_collection(name=collection_name)
    results=coll.query(query_texts=[ip],n_results=15)
